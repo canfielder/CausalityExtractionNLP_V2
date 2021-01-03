840c025e9264e578a8477e030b1080d9b815f5be
Evan Canfield <38056064+canfielder@users.noreply.github.com>
2021-01-03 16:04:01 -0500

    Initial Commit - Manual Upload

---
+++ R/install_packages.R
@@ -0,0 +1,25 @@
+#' The following script is used for ensuring packages are installed
+#' across the entire project. This script provides a single location
+#' for documenting which packages are used. Any individual script or
+#' notebook only needs to load packages, not install.
+#
+
+project_install_packages <- function(){
+  if (!require(pacman)) {install.packages('pacman')}
+  p_load(
+    dplyr,
+    fastTextR,
+    lime,
+    pdftools,
+    quanteda,
+    readxl,
+    reticulate,
+    rtika,
+    stringr,
+    tabulizer,
+    textstem,
+    tidyr,
+    tidytext,
+    tokenizers
+  )
+}
