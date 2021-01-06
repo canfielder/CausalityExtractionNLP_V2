4f7de1063e7ad05995c21417ab49340f6033f4a0
Evan Canfield <canfielder@gmail.com>
2021-01-06 09:07:41 -0500

    General Update

---
+++ R/entity_extraction.R
@@ -0,0 +1,13 @@
+#' The following script stores the functions related to the Entity Extraction
+#' actions in the Causality Extraction NLP project.
+#
+
+# Library ---------------------------------------------------------------------
+
+
+# Functions -------------------------------------------------------------------
+
+#' Convert Prediction Classes
+#'
+#' The Entity Extraction model returns predictions as a two dimensional array
+#
