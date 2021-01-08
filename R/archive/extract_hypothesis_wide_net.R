fa58c5b7b549a719d0a5da009891ec49a8da2d7c
Evan Canfield <canfielder@gmail.com>
2021-01-07 21:00:13 -0500

    Add entity trimmiing work to Entity Extraction

--- R/archive/extract_hypothesis_wide_net.R
+++ R/archive/extract_hypothesis_wide_net.R
@@ -11,9 +11,13 @@ p_load(
 )
 
 # REGEX Strings ---------------------------------------------------------------
+## Identify Letters
+### Wide Net
+# regex_hypothesis <- "(hypo)|(h\\d+)"
 
-## Identify Hypo _ #:
-regex_hypo_marker <- "<split>hypo (.*?):"
+### Narrow Scope
+regex_hypothesis <- "(hypo )"
+
 
 # Functions -------------------------------------------------------------------
 
@@ -38,61 +42,36 @@ extract_hypothesis <- function(input_text){
   # Normalize Text ------------------------------------------------------------
   processing_text <- tolower(processing_text)
 
-  
-  # Hypothesis Extraction -----------------------------------------------------
-  # Identify Lines with Hypothesis Pattern
-  h_match <- processing_text %>% str_match(regex_hypo_marker)
-  
-  # Extract Hypotheses Number
-  h_match_num <- h_match[,2]
-  
-  # Identify Unique Hypothesis Numbers
-  h_match_num_unq <- unique(h_match_num)
-  
-  # Drop NA
-  h_match_num_unq <- h_match_num_unq[!is.na(h_match_num_unq)]
-  
-  # Determine Vector Index of Initial Hypothesis Statements
-  h_initial <- c()
-  for (i in h_match_num_unq){
-    intial_idx <- tapply(seq_along(h_match_num),
-                         h_match_num,
-                         min)[i]
-    h_initial <- c(h_initial, intial_idx)
-  }
-  
-  # Reduce Text to Only Initial Hypothesis Instances
-  h_statements <- processing_text[h_initial]
-  
-  
+  # Identify Hypothesis Statements --------------------------------------------
+  ## Return Logical Vector
+  logical_hypothesis_1 <- str_detect(processing_text, regex_hypothesis)
+
+  ## Reduce Document to Only Hypothesis Statements
+  hypothesis_statements <- processing_text[logical_hypothesis_1]
+
   # Split Statements On Indicator (Defined in Processing) ---------------------
   ## Define
   split_indicator <- "<split>"
 
   ## Split on Indicator
-  h_statements <- str_split(string = h_statements,
+  hypothesis_statements <- str_split(string = hypothesis_statements,
                                      pattern = split_indicator) %>%
     unlist()
 
   ## Detect Statements Which Contain "Hypo"
-  logical_hypothesis_2 <- str_detect(h_statements, "hypo")
+  logical_hypothesis_2 <- str_detect(hypothesis_statements, "hypo")
 
   ## Drop Statements that Do Not Include "Hypo"
-  h_statements <- h_statements[logical_hypothesis_2]
-  
-  # Drop ~Hypo #:~
-  h_statements <- gsub(".*: ","",h_statements)
-  
-  # Drop Empty Strings
-  h_statements <- h_statements[h_statements != ""]
-  
+  hypothesis_statements <- hypothesis_statements[logical_hypothesis_2]
+
   # Create Dataframe with Hypothesis Number and Hypothesis
-  df_hypothesis <- as.data.frame(h_statements,
+  df_hypothesis <- as.data.frame(hypothesis_statements,
                                  stringsAsFactors = FALSE)
+
 
   # Rename and add Hypothesis Number
   df_hypothesis <- df_hypothesis %>%
-    rename(hypothesis = h_statements) %>%
+    rename(hypothesis = hypothesis_statements) %>%
     mutate(
       h_id = paste0("h_", row_number())
     ) %>%
