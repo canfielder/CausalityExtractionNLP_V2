2b27849a24a3ad5ec3fad387d313739f4c814235
Evan Canfield <canfielder@gmail.com>
2021-01-10 12:30:55 -0500

    Relocate Shiny work to seperate repo

--- shiny_CausalityExtractionNLP/www/R/install_packages.R
+++
@@ -1,9 +0,0 @@
-#' The following script is used to load all libraries used.
-
-shiny_install_packages <- function(){
-  library(DT)
-  library(fastTextR)
-  library(lime)
-  library(readxl)
-  library(shiny)
-}
